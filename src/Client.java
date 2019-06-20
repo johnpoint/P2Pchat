5a93d57b4ea80f32d592e6ae57eba5fb880c29c7
johnpoint <me@lvcshu.com>
2019-06-20 20:38:15 +0800

    update

--- src/Client.java
+++ src/Client.java
@@ -3,19 +3,30 @@ import java.io.*;
 
 public class Client {
 
-    private String peerClientAddr;
-    final private String peerClientPort = "5118";
+    private String peerClientAddr = "0.0.0.0";
+    final private int peerClientPort = 5118;
 
     public Client(String peerClientAddr) {
         this.peerClientAddr = peerClientAddr;
     }
 
-    /*public void sendMessage(String text){
+    public int sendMessage(String text) {
+        if (this.peerClientAddr == "0.0.0.0") {
+            return 1;
+        }
+        Socket client = new Socket(this.peerClientAddr, peerClientPort);
+        OutputStream outToServer = client.getOutputStream();
+        DataOutputStream out = new DataOutputStream(outToServer);
+        out.writeUTF(text);
+        InputStream inFromServer = client.getInputStream();
+        DataInputStream in = new DataInputStream(inFromServer);
+        System.out.println(in.readUTF());
+        client.close();
+        return 0;
+    }
 
-    }*/
-
-    public void debug(){
-        System.out.println(peerClientAddr);
-        System.out.println(peerClientPort);
+    public void debug() {
+        System.out.println(this.peerClientAddr);
+        System.out.println(this.peerClientPort);
     }
 }
