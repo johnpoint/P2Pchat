9316e60588ec43921b69390521729cf34cac9f3a
johnpoint <me@lvcshu.com>
2019-06-27 20:30:50 +0800

    update

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,17 @@
 import java.net.*;
 import java.io.*;
 
-public class Client implements java.io.Serializable {
+public class Client extends Thread {
+    private Thread t;
+    public Inter chat;
+    public String Text;
 
     private String peerClientAddr = "8.8.8.8";
     final private int peerClientPort = 5117;
 
-    public Client(String peerClientAddr) {
+    public Client(String peerClientAddr, Inter chat) {
         this.peerClientAddr = peerClientAddr;
+        this.chat = chat;
     }
 
     public int sendMessage(String text) throws IOException {
@@ -27,6 +31,34 @@ public class Client implements java.io.Serializable {
         } catch (IOException e) {
             return 1;
         }
+    }
+
+    public void setText(String Text) {
+        this.Text = Text;
+    }
+
+    public void run() {
+        Socket client;
+        OutputStream outToServer;
+        DataOutputStream out;
+        if (this.peerClientAddr == "8.8.8.8") {
+            return;
+        }
+        try {
+            client = new Socket(this.peerClientAddr, peerClientPort);
+            outToServer = client.getOutputStream();
+            out = new DataOutputStream(outToServer);
+            out.writeUTF(this.Text);
+            client.close();
+            return;
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+    public void start() {
+        t = new Thread(this);
+        t.start();
     }
 
     public void debug() {
